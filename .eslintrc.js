9c88f8b49e99ab4e7bcf260d225621687b1e9bc6
Daniell Marafon <daniellmarafon@gmail.com>
2022-05-30 13:53:01 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+  },
+};
