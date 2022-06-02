95d239091e95d9c0a4faab73eaa603a84668bdb3
Daniell Marafon <daniellmarafon@gmail.com>
2022-06-02 17:40:10 +0200

    Adding React Portal Dom

---
+++ src/components/ReactPortalDom/ReactPortalDom.tsx
@@ -0,0 +1,51 @@
+import { useState, useLayoutEffect } from "react";
+import { createPortal } from "react-dom";
+
+declare module "react" {
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    SetStateAction?: string;
+  }
+}
+
+const createWrapperAndAppendToBody = (wrapperId: string) => {
+  const wrapperElement = document.createElement("div");
+  wrapperElement.setAttribute("id", wrapperId);
+  wrapperElement.setAttribute("data-testid", "custom-element");
+  document.body.appendChild(wrapperElement);
+  return wrapperElement;
+};
+
+const ReactPortal = ({
+  children,
+  wrapperId = "react-portal-wrapper",
+}: {
+  children: any;
+  wrapperId: string;
+}) => {
+  const [wrapperElement, setWrapperElement] = useState<HTMLElement | null>(
+    null
+  );
+
+  useLayoutEffect(() => {
+    let element = document.getElementById(wrapperId) as HTMLElement;
+    let systemCreated = false;
+
+    if (!element) {
+      systemCreated = true;
+      element = createWrapperAndAppendToBody(wrapperId);
+    }
+    setWrapperElement(element);
+
+    return () => {
+      if (systemCreated && element.parentNode) {
+        element.parentNode.removeChild(element);
+      }
+    };
+  }, [wrapperId]);
+
+  if (wrapperElement === null) return null;
+
+  return createPortal(children, wrapperElement);
+};
+
+export default ReactPortal;
