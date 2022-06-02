d922bc76da8f61598692dcf947424867f4f15c0e
Daniell Marafon <daniellmarafon@gmail.com>
2022-06-02 16:22:51 +0200

    Adding fix of code smells

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,3 +1,4 @@
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
