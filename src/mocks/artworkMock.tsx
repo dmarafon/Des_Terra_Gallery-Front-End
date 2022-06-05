4beaa19150877447c7a0ae194d5221799435e55f
Daniell Marafon <daniellmarafon@gmail.com>
2022-06-05 21:20:03 +0200

    Removing 2 mock objects due to sonar cloud

--- src/mocks/artworkMock.tsx
+++ src/mocks/artworkMock.tsx
@@ -19,44 +19,4 @@ export const artworkMock = [
     image: "https://ibb.co/mht27wM",
     id: "6294aa4bc78dbede94290071",
   },
-  {
-    title: "constructor i",
-    medium: "drawing on paper",
-    height: "23 inches",
-    width: "19 inches",
-    style: "drawing",
-    description:
-      "Moving through Barcelona, sometimes I feel asfixiated by the buildings and the city smoke, but at the same time, the beautiful golden sun that bathes us warms me. Trying to mix those elements I created this piece.",
-    author: [
-      {
-        surname: "perea",
-        firstname: "jesus",
-        id: "6295020ad1504446d0c04ce8",
-      },
-    ],
-    purchaseprice: "150",
-    monthlyrateprice: "10",
-    image: "https://ibb.co/VN03vPk",
-    id: "6294aa4bc78dbede94290072",
-  },
-  {
-    title: "modales",
-    medium: "drawing on paper",
-    height: "22 inches",
-    width: "18 inches",
-    style: "drawing",
-    description:
-      "I've always been fascinated by modals, in music, programming and any other fields and their concepts. The idea of something that can be modularized and used in other places, like an extension of life itself...",
-    author: [
-      {
-        surname: "perea",
-        firstname: "jesus",
-        id: "6295020ad1504446d0c04ce8",
-      },
-    ],
-    purchaseprice: "140",
-    monthlyrateprice: "15",
-    image: "https://ibb.co/album/RY29BK",
-    id: "6294aa4bc78dbede94290073",
-  },
 ];
