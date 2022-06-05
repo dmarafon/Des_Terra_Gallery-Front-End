1aebfff218812ca67804c6ce2ef21cd5f3b89320
Daniell Marafon <daniellmarafon@gmail.com>
2022-06-05 21:14:14 +0200

    Adding arworks slice test

---
+++ src/redux/features/artworkSlice.test.tsx
@@ -0,0 +1,21 @@
+import { artworkMock } from "../../mocks/artworkMock";
+import { IArtworks } from "../../types/artworksInterface";
+import recordsSlice, { loadartworksActionCreator } from "./artworkSlice";
+
+describe("Given a artworksReducer", () => {
+  describe("When it receives an initial state and a loadArtworks action with 3 arts as payload", () => {
+    test("Then it should return a new records state with the 3 art objecs and an array of the 3 arts", () => {
+      const artworkPayload = artworkMock;
+
+      const initialStatus: IArtworks[] = [];
+
+      const expectedNewState = [...artworkPayload];
+
+      const loadArworkAction = loadartworksActionCreator(artworkPayload);
+
+      const newState = recordsSlice(initialStatus, loadArworkAction);
+
+      expect(newState).toEqual(expectedNewState);
+    });
+  });
+});
