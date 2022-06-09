f1e5f0cb59700ce1a8116ca6a66ef59e37855a4c
Daniell Marafon <daniellmarafon@gmail.com>
2022-06-09 23:32:08 +0200

    Adding more tests for Register

---
+++ src/components/RegisterForm/RegisterFormErrorSuite.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "../../redux/store/store";
+import RegisterForm from "./RegisterForm";
+
+const mockDispatch = jest.fn();
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: () => mockDispatch,
+}));
+
+describe("Given a RegisterForm component function", () => {
+  describe("When the user does not type in any of the required  clicks the checkbox", () => {
+    test("Then an action is dispatched opening a modal to warn the user", async () => {
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <RegisterForm />
+          </Provider>
+        </BrowserRouter>
+      );
+      const registerButton = screen.getByRole("button", { name: "REGISTER" });
+      userEvent.click(registerButton);
+      await waitFor(() => {
+        const uIaction = {
+          type: "ui/apiResponse",
+          payload: "Blank",
+        };
+        store.dispatch(uIaction);
+      });
+      expect(mockDispatch).toHaveBeenCalled();
+      const closeModal = screen.getByRole("button", {
+        name: /×/i,
+      });
+      userEvent.click(closeModal);
+      const element = screen.getByTestId("custom-element");
+      expect(element).toBeInTheDocument();
+    });
+  });
+});
--- src/mocks/handlers.tsx
+++ src/mocks/handlers.tsx
@@ -9,16 +9,6 @@ export const handlers = [
   rest.post(`${process.env.REACT_APP_API_URL}users/register`, (req, res, ctx) =>
     res(ctx.status(200), ctx.json({ token: mockToken }))
   ),
-  rest.get(`${process.env.REACT_APP_API_URL}artworks/all`, (req, res, ctx) =>
-    res(ctx.status(200))
-  ),
-  rest.get(`${process.env.REACT_APP_API_URL}artworks/myart/`, (req, res, ctx) =>
-    res(ctx.status(200), ctx.json({ token: mockToken }))
-  ),
-  rest.delete(
-    `${process.env.REACT_APP_API_URL}artworks/myart/`,
-    (req, res, ctx) => res(ctx.status(200), ctx.json({ token: mockToken }))
-  ),
   rest.post(
     `${process.env.REACT_APP_API_URL}artworks/addart`,
     (req, res, ctx) => res(ctx.status(200), ctx.json({ token: mockToken }))
