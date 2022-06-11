b7dadb34bd9ab8ce839cd8bd83d10592409bd973
Daniell Marafon <daniellmarafon@gmail.com>
2022-06-11 02:19:07 +0200

    First tests completed, covering lines

---
+++ src/redux/features/singleArtworkSlice.test.tsx
@@ -0,0 +1,41 @@
+import { artworkMock } from "../../mocks/artworkMock";
+import { IArtworks } from "../../types/artworksInterface";
+import artworksSlice, { loadartworksActionCreator } from "./artworkSlice";
+import { loadSingleArtworkActionCreator } from "./singleArtworkSlice";
+
+describe("Given a singleArtworkReduces", () => {
+  describe("When it receives an initial state and a loadSingleArtwork action with 1 art as payload", () => {
+    test("Then it should return a new records state with the 3 art objecs and an array of the 3 arts", () => {
+      const artworkPayload = artworkMock[0];
+
+      const initialStatus = {
+        title: "concreto",
+        medium: "drawing on paper",
+        height: "20 inches",
+        width: "17.5 inches",
+        style: "drawing",
+        description:
+          "When I was in Brazil I was inspired but a cultural movement called 'Concretismo'. With its angular and blocked shapes, sorrounded by Neymaier architecture, and a retro-futuristic aesthetic, I tried achieve this piece about a nostalgi future.",
+        author: [
+          {
+            surname: "perea",
+            firstname: "jesus",
+          },
+        ],
+        purchaseprice: "120",
+        monthlyrateprice: "10",
+        image: "https://ibb.co/mht27wM",
+        imagebackup: "https://ibb.co/mht27wM",
+        id: "6294aa4bc78dbede94290071",
+      };
+
+      const expectedNewState = { ...initialStatus };
+
+      const loadArworkAction = loadSingleArtworkActionCreator(initialStatus);
+
+      const newState = artworksSlice(initialStatus, loadArworkAction);
+
+      expect(newState).toEqual(expectedNewState);
+    });
+  });
+});
