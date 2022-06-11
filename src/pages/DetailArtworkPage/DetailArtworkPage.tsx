48e711490da4afb3a34789b7ac03101e7c3e50d2
Daniell Marafon <daniellmarafon@gmail.com>
2022-06-11 19:29:44 +0200

    Fixed tests after minor changes

--- src/pages/DetailArtworkPage/DetailArtworkPage.tsx
+++ src/pages/DetailArtworkPage/DetailArtworkPage.tsx
@@ -1,6 +1,3 @@
-import { title } from "process";
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
 import DetailArtwork from "../../components/DetailArtwork/DetailArtwork";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
