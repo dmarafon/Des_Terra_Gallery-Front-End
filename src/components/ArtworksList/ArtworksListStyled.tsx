1a4c5148668c81f1febfbccd82c8ff2009718035
Daniell Marafon <daniellmarafon@gmail.com>
2022-06-12 13:46:41 +0200

    Adding responsive navigation

--- src/components/ArtworksList/ArtworksListStyled.tsx
+++ src/components/ArtworksList/ArtworksListStyled.tsx
@@ -17,13 +17,42 @@ const ArtworksListStyled = styled.div`
     align-items: center;
     justify-content: center;
     padding-inline-start: 0;
-    cursor: pointer;
   }
-  .artworks__navigate--button {
+  .artworks__navigate--button_first {
     filter: invert(61%) sepia(10%) saturate(5710%) hue-rotate(335deg)
       brightness(102%) contrast(96%);
+    cursor: pointer;
+    width: 10vh;
+    margin-right: 100px;
+    @media (max-width: 995px) {
+      position: fixed;
+      left: 5%;
+      bottom: 40%;
+      z-index: 1000;
+      width: 8vh;
+    }
+  }
 
-    width: 90px;
+  .artworks__navigate--button_second {
+    filter: invert(61%) sepia(10%) saturate(5710%) hue-rotate(335deg)
+      brightness(102%) contrast(96%);
+    cursor: pointer;
+    width: 10vh;
+    margin-left: 100px;
+    @media (max-width: 995px) {
+      position: fixed;
+      right: 5%;
+      bottom: 40%;
+      z-index: 1000;
+      width: 8vh;
+    }
+  }
+
+  .artworks__navigate--counter {
+    color: #fa803b;
+    font-weight: bolder;
+    font-size: 200%;
+    cursor: default;
   }
 `;
 export default ArtworksListStyled;
